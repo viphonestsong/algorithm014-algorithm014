20369b25bf44280200e4e9569c00197855746aed
songhaoyu <14089998@qq.com>
2020-09-20 23:12:22 +0800

    week6

---
+++ Week_06/DecodeWays.java
@@ -0,0 +1,34 @@
+public class DecodeWays {
+
+    public static void main(String[] args) {
+        String s = "1234";
+        System.out.println(s.substring(1, 2));
+    }
+
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        if (s.charAt(n - 1) == '0') {
+            dp[n - 1] = 0;
+        } else {
+            dp[n - 1] = 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            if ((s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0') <= 26) {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+        return dp[0];
+    }
+
+}
